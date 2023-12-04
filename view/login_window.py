1155d20e95013c17ec4dfd385692ddbe58b26b5c
Bubbl33s <jairo.lf.8@gmail.com>
2023-12-04 10:47:31 -0500

    sync

--- view/login_window.py
+++ view/login_window.py
@@ -5,4 +5,4 @@ class LoginWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(LoginWindow, self).__init__(parent)
         uic.loadUi("ui/login_window.ui", self)
-        self.
+        self.show()
